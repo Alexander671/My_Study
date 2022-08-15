52c753fdc96d3b46c0b51683b4a9805bc24e5e97
Alexander671 <matveevalex97@yandex.ru>
2022-08-15 22:01:19 +0300

    4.4.10

--- STEPIK/Python_OOP/4/4.4/9.py
+++ STEPIK/Python_OOP/4/4.4/9.py
@@ -1,58 +1,35 @@
-class Aircraft:
-    def __init__(self, model, mass, speed, top):
-        self.verify_types(model, mass, speed, top)
-        self._model = model
-        self._mass = mass
-        self._speed = speed
-        self._top = top
 
-    def verify_types(self, model, mass, speed, top):
-        for i in [mass, speed, top]:
-            self.verify_positive(i)
-        if not type(model) == str:
-            raise TypeError('неверный тип аргумента')
+def integer_params_decorated(vector, name, fn):
+    def wrapper(*args, **kwargs):
+        vector.append(name)
+        return fn(*args, **kwargs)
+    return wrapper
 
-
-    def verify_positive(self, number):
-        if type(number) == int or type(number) == float:
-            if number < 0:
-                raise TypeError('неверный тип аргумента')
-        else:
-            raise TypeError('неверный тип аргумента')
-
-    def verify_str(self, string):
-        if type(string) != str:
-            raise TypeError('неверный тип аргумента')
-
-    def verify_int(self, number):
-        if type(number) == int:
-            if number < 0:
-                raise TypeError('неверный тип аргумента')
-        else:
-            raise TypeError('неверный тип аргумента')
-
-class PassengerAircraft(Aircraft): # - пассажирский самолет;
-    def __init__(self, model, mass, speed, top, chairs):
-        super().__init__(model, mass, speed, top,)
-        self.verify_int(chairs)
-        self._chairs = chairs
-
-
-class WarPlane(Aircraft): # - военный самолет.
-    def __init__(self, model, mass, speed, top, weapons:dict):
-        super().__init__(model, mass, speed, top,)
-        if type(weapons) != dict:
-            raise TypeError('неверный тип аргумента')
-
-        map(lambda x: self.verify_positive(x), (weapons.values()))
-        map(lambda x: self.verify_str(x), (weapons.keys()))
+def class_log(vector):
+    def wrapper(cls):
         
-        self._weapons = weapons
+        methods = {k: v for k, v in cls.__dict__.items() if callable(v)}
+        for k, v in methods.items():
+            setattr(cls, k, integer_params_decorated(vector, k, v))
+        return cls    
+    return wrapper
 
 
+vector_log = []
 
-obj1 = PassengerAircraft('МС-21', 1250, 8000, 12000.5, 140)
-obj2 = PassengerAircraft('SuperJet', 1145, 8640, 11034, 80)
-obj3 = WarPlane('Миг-35', 7034, 25000, 2000, {"ракета": 4, "бомба": 10})
-obj4 = WarPlane('Су-35', 7034, 34000, 2400, {"ракета": 4, "бомба": 7})
-planes = [obj1, obj2, obj3, obj4]
+
+@class_log(vector_log)
+class Vector:
+    def __init__(self, *args):
+        self.__coords = list(args)
+
+    def __getitem__(self, item):
+        return self.__coords[item]
+
+    def __setitem__(self, key, value):
+        self.__coords[key] = value
+
+
+v = Vector(1, 2, 3)
+v[0] = 10
+print(vector_log)
