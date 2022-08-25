e9e8a42ec4284188cce450f154cc787faaacdf52
Alexander671 <matveevalex97@yandex.ru>
2022-08-25 21:42:02 +0300

    5.1.10

--- STEPIK/Python_OOP/2/2.3/7.py
+++ STEPIK/Python_OOP/2/2.3/7.py
@@ -7,20 +7,29 @@ class ValidateString:
         return (type(string) == str) and (self.min_length <= len(string) <= self.max_length)
 
 class StringValue:
-    def __init__(self, validator):
+    def __init__(self, validator:ValidateString):
         self.validator = validator
 
-st = StringValue(validator=ValidateString(3, 100))
+    def __get__(self, instance, owner):
+        return instance.__dict__[self.name]
 
+    def __set_name__(self, owner, name):
+        self.name = "_" + name
+
+    def __set__(self, instance, value):
+        if self.validator.validate(value):
+            instance.__dict__[self.name] = value
+
+        
+st = StringValue(validator=ValidateString(3, 100))
 validate = ValidateString(min_length=3, max_length=100)
 
 
-
-
 class RegisterForm:
-    login = StringValue() # - для ввода логина;
-    password = StringValue() #  - для ввода пароля;
-    email = StringValue()  # - для ввода Email.
+    login = StringValue(validate) # - для ввода логина;
+    password = StringValue(validate) #  - для ввода пароля;
+    email = StringValue(validate)  # - для ввода Email.
+
     def __init__(self, login, password, email):
         self.login = login # - для ввода логина;
         self.password = password #  - для ввода пароля;
@@ -37,4 +46,38 @@ class RegisterForm:
 Email: {self.email}
 </form>''') 
 
-form = RegisterForm('login', 'password', 'email')
+assert hasattr(ValidateString, 'validate'), "в классе ValidateString отсутствует метод validate"
+
+r = RegisterForm('11111', '1111111', '11111111')
+assert hasattr(r,'login') and hasattr(r, 'password') and hasattr(r, 'email'), "в классе RegisterForm должны быть дескрипторы login, password, email"
+
+assert hasattr(RegisterForm, 'show'), "в классе RegisterForm отсутствует метод show"
+
+StringValue.__doc__
+
+frm = RegisterForm("123", "2345", "sc_lib@list.ru")
+assert frm.get_fields() == ["123", "2345", "sc_lib@list.ru"], "метод get_fields вернул неверные данные"
+
+frm.login = "root"
+assert frm.login == "root", "дескриптор login вернул неверные данные"
+
+v = ValidateString(5, 10)
+assert v.validate("hello"), "метод validate вернул неверное значение"
+assert v.validate("hell") == False, "метод validate вернул неверное значение"
+assert v.validate("hello world!") == False, "метод validate вернул неверное значение"
+
+
+class A:
+    st = StringValue(validator=ValidateString(3, 10))
+
+
+a = A()
+a.st = "hello"
+
+assert a.st == "hello", "дескриптор StringValue вернул неверное значение"
+a.st = "d"
+assert a.st == "hello", "дескриптор StringValue сохранил строку длиной меньше min_length"
+a.st = "dапарпаропропропропр"
+assert a.st == "hello", "дескриптор StringValue сохранил строку длиной больше max_length"
+a.st = "dапарпароп"
+assert a.st == "dапарпароп", "дескриптор StringValue сохранил строку длиной больше max_length"
