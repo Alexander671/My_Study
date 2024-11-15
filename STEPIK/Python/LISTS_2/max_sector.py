f6df5e6b1f425615109e707a001704406fa050aa
Alexander671 <matveevalex97@yandex.ru>
2024-11-15 13:23:23 +0300

    Remove deleted files from index

--- STEPIK/Python/LISTS_2/max_sector.py
+++
@@ -1,8 +0,0 @@
-n = int(input())
-list_sector = []
-for i in range(n):
-    s = (list(map(lambda x : int(x), input().split())))
-    for j in range(n):
-        if (i >= j):
-            list_sector.append(s[j])
-print(max(list_sector))
