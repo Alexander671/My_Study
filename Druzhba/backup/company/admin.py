bd9bfcc32748311da5ebf32d775834150436587e
Александр Матвеев <aleksandr.matveev@tetrika.school>
2025-04-25 14:21:40 +0300

    COMPLETELY REMOVED large files and their history

--- Druzhba/backup/company/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from company.models import CompanyMember, Company
-
-admin.site.register(CompanyMember)
-admin.site.register(Company)
--- Druzhba/backup/core/redis.py
+++
@@ -1,5 +0,0 @@
-import redis
-
-from .settings import REDIS_URL
-
-conn = redis.Redis.from_url(REDIS_URL)
